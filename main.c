f5c681d0970c0c572711cedbcd160e7d92c56c29
Aniket Patil <63067583+Aniket404@users.noreply.github.com>
2025-02-16 15:45:49 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,20 +1,34 @@
-#include "stm32f4xx.h"
+#include "tim.h"
 
-#define GPIOAEN (1U<<0)
-#define PIN5    (1U<<5)
-#define LED_PIN  PIN5
 
-int main(){
-	/*ENABLE ACCESS TO GPIOA*/
-	RCC->AHB1ENR|=GPIOAEN;
-	/*SET PA5TO OP MODE*/
-	GPIOA->MODER|=(1U<<10);
-	GPIOA->MODER&=~(1U<<11);
-	while(1){
-		GPIOA->ODR^=LED_PIN;
-		for(int i=0;i<100000;i++){
 
+int main(void)
+{
+	/*Init pwm at pa0 and pa1*/
+	tim2_pa0_pa1_pwm_init();
+
+	/*test 1*/
+	//tim2_pa0_pa1_pwm_set_dutycycle(1,100);
+	//tim2_pa0_pa1_pwm_set_dutycycle(2,1);
+
+	while(1)
+	{
+		/*test 2*/
+
+		for( int i = 0; i < 100; i++)
+		{
+			tim2_pa0_pa1_pwm_set_dutycycle(1,i);
+			tim2_pa0_pa1_pwm_set_dutycycle(2,100 - i);
+			pseudo_dly();
 		}
 
+		for( int i = 0; i < 100; i++)
+		{
+			tim2_pa0_pa1_pwm_set_dutycycle(1,100 - i);
+			tim2_pa0_pa1_pwm_set_dutycycle(2,i);
+			pseudo_dly();
+
+		}
 	}
+
 }
