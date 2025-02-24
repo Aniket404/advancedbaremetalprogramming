207ba725a25078240e7a48d8a8a4023f9cd2543f
Aniket Patil <63067583+Aniket404@users.noreply.github.com>
2025-02-23 22:53:16 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,34 +1,61 @@
-#include "tim.h"
+#include "stm32f4xx.h"
+#include <stdio.h>
+#include "dma.h"
+#include "uart_dma.h"
+#include "adc_dma.h"
+#include "mpu9250.h"
+#include <string.h>
 
+
+
+
+float acc_x,acc_y,acc_z;
 
 
 int main(void)
 {
-	/*Init pwm at pa0 and pa1*/
-	tim2_pa0_pa1_pwm_init();
 
-	/*test 1*/
-	//tim2_pa0_pa1_pwm_set_dutycycle(1,100);
-	//tim2_pa0_pa1_pwm_set_dutycycle(2,1);
+
+	/*Enable SPI*/
+	spi1_dma_init();
+
+	/*Config NCS pin*/
+	mpu9250_ncs_pin_config();
+
+	/*Enable Floating Point Unit*/
+	SCB->CPACR |= ((3UL << 10*2)|(3UL << 11*2));
+
+	/*Enable tx stream*/
+	dma2_stream3_spi_tx_init();
+
+	/*Enable rx stream*/
+	dma2_stream2_spi_rx_init();
+
+	/*Reset NCS pin*/
+	mpu9250_ncs_pin_reset();
+
+	/*Config accel*/
+	mpu9250_accel_config(ACC_FULL_SCALE_2_G);
 
 	while(1)
 	{
-		/*test 2*/
 
-		for( int i = 0; i < 100; i++)
-		{
-			tim2_pa0_pa1_pwm_set_dutycycle(1,i);
-			tim2_pa0_pa1_pwm_set_dutycycle(2,100 - i);
-			pseudo_dly();
-		}
+		/*Reset NCS pin*/
+		mpu9250_ncs_pin_reset();
 
-		for( int i = 0; i < 100; i++)
-		{
-			tim2_pa0_pa1_pwm_set_dutycycle(1,100 - i);
-			tim2_pa0_pa1_pwm_set_dutycycle(2,i);
-			pseudo_dly();
+        /*Update accel values*/
+		mpu9250_accel_update();
 
-		}
+		/*Get accel data*/
+		acc_x =  mpu9250_get_x();
+		acc_y =  mpu9250_get_y();
+		acc_z =  mpu9250_get_z();
+
+		/*Set NCS pin*/
+		mpu9250_ncs_pin_set();
 	}
-
 }
+
+
+
+
